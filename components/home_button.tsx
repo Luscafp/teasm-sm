5f00b2f45b9a6d654d0370b2ccd3d02b2e9eb58f
Luscafp <lucasfariaslfp@gmail.com>
2025-06-09 20:39:02 -0300

    fix: home_button corrigido

--- components/home_button.tsx
+++ components/home_button.tsx
@@ -1,22 +1,22 @@
 import { useRouter } from 'expo-router';
-import { TouchableOpacity, Image, View } from 'react-native';
+import { Pressable, Image, View } from 'react-native';
 
 const HomeButton = () => {
   const router = useRouter();
 
   const handlePress = () => {
-    router.push('Escolha01');
+    router.push('/start/escolha_01');
   };
 
   return (
-    <TouchableOpacity 
+    <Pressable 
       onPress={handlePress}
       className="absolute bottom-0 w-full bg-accent h-[10%] rounded-t-xl justify-center items-center"
     >
       <View className="w-full h-full justify-center items-center">
         <Image source={require('../assets/home.png')} />
       </View>
-    </TouchableOpacity>
+    </Pressable>
   );
 };
 
